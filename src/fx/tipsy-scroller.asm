dc7e67e7fbb28f10ca8ecce4c1c466d6824e0ac1
Kieran Connell <kieran@kconnell.co.uk>
2025-06-01 10:13:03 +0100

    Optimise scroller.

--- src/fx/tipsy-scroller.asm
+++ src/fx/tipsy-scroller.asm
@@ -95,17 +95,35 @@ tipsy_scroller_tick:
 
     sub r0, r0, #8
     ldr r12, tipsy_scroller_text_p
-    add r12, r12, #1
-    ldrb r1, [r12]
+    ; Character loop.
+    .11:
+    ldrb r1, [r12], #1
     cmp r1, #0
     ldreq r12, tipsy_scroller_base_p
+    beq .11
     str r12, tipsy_scroller_text_p
+
+    ; Copy glyph column into a shift buffer.
+    cmp r1, #0x60
+    bicgt r1, r1, #0x20             ; force upper case
+    sub r1, r1, #32                 ; ascii space
+
+    ldr r11, tipsy_scroller_font_p
+    add r11, r11, r1, lsl #5        ; assumes 32 bytes per glyph.
+
+    adr r10, tipsy_scroller_column_buffer
+    ldmia r11, {r2-r9}
+    stmia r10, {r2-r9}
 
 .1:
     str r0, tipsy_scroller_column
     ldr pc, [sp], #4
 
+; ============================================================================
+; SLOW VERSION.
+; ============================================================================
 
+.if 0
 ; R12=screen addr ptr.
 tipsy_scroller_draw:
     str lr, [sp, #-4]!
@@ -289,11 +307,95 @@ tipsy_scroller_draw:
     bpl .1                          ; 41 columns!
 
     ldr pc, [sp], #4
+.endif
 
 ; font word = 0xabcdefgh
 ; scroll by 1 pixel = 0x0abcdefg - shift right by 4 for second word.
 ; scroll by 1 pixel = 0xi0000000 - shift left by 28 for first word.
 
+; ============================================================================
+; FAST VERSION.
+; ============================================================================
+
+.macro scroller_shift_left_by_pixels
+	; shift word right 4 bits to clear left most pixel
+	mov r0, r0, lsr r12
+	; mask in right most pixel from next word
+	orr r0, r0, r1, lsl r14
+    ; etc.
+	mov r1, r1, lsr r12
+	orr r1, r1, r2, lsl r14
+	mov r2, r2, lsr r12
+	orr r2, r2, r3, lsl r14
+	mov r3, r3, lsr r12
+	orr r3, r3, r4, lsl r14
+	mov r4, r4, lsr r12
+	orr r4, r4, r5, lsl r14
+	mov r5, r5, lsr r12
+	orr r5, r5, r6, lsl r14
+	mov r6, r6, lsr r12
+	orr r6, r6, r7, lsl r14
+	mov r7, r7, lsr r12
+	orr r7, r7, r8, lsl r14
+.endm
+
+; R9=dst line address
+; R10=src line address
+; R11=right hand word ptr
+; R12=pixel shift
+tipsy_scroller_scroll_line:
+	str lr, [sp, #-4]!
+    rsb r14, r12, #32       ; reverse pixel shift (lsl #32-4*n)
+
+    .rept (Screen_Width/64)-1
+	ldmia r10, {r0-r8}		; read 9 words = 36 bytes = 72 pixels
+    add r10, r10, #4*8      ; move 8 words
+    scroller_shift_left_by_pixels
+	stmia r9!, {r0-r7}		; write 8 words = 32 bytes = 64 pixels
+    .endr
+
+    ; Last block!
+	ldmia r10!, {r0-r7}		; read 8 words = 32 bytes = 64 pixels
+    ldr r8, [r11]
+    scroller_shift_left_by_pixels
+	stmia r9!, {r0-r7}		; write 8 words = 32 bytes = 64 pixels
+
+	mov r8, r8, lsr r12	    ; rotate new data word
+	str r8, [r11], #4      ; scroller_glyph_column_buffer[r11]=r10
+	ldr pc, [sp], #4
+
+; R12=screen addr
+tipsy_scroller_draw_fast:
+	str lr, [sp, #-4]!
+
+    ; R9=dst line address
+    ldr r0, tipsy_scroller_y_pos
+    add r9, r12, r0, lsl #7
+    add r9, r9, r0, lsl #5          ; assume stride is 160.
+
+    ; R10=src line address
+    mov r10, r9                     ; not double buffered.
+
+    ; R11=right hand word ptr
+    adr r11, tipsy_scroller_column_buffer
+
+    ; R12=pixel shift
+    ldr r12, tipsy_scroller_speed
+    mov r12, r12, lsl #2    ; pixel shift (lsr #4*n)
+
+	.rept 8
+	bl tipsy_scroller_scroll_line
+    .endr
+
+	ldr pc, [sp], #4
+
+; ============================================================================
+
+tipsy_scroller_column_buffer:
+    .skip 8*4
+
+; ============================================================================
+; SCROLLTEXT TEXT. TODO: Move to data segment.
 ; ============================================================================
 
 tipsy_scroller_message_no_adr:
